3d05f3756775d10ec4b12fd6550576a8be98d6ca
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-10-29 11:53:33 +0900

    画像、他カラムの保存を許可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :category_id, :condition_id, :fee_id, :prefecture_id, :scheduled_id, :image).merge(user_id: current_user.id)
 end
