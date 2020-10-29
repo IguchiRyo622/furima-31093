39d2ad7e99eb44b864ba22113d7a6fe03be38b22
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-10-29 12:11:10 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :info
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :scheduled_id
+    validates :image
+  end
 end
