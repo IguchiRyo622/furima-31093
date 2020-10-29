552ef885dc406fa32dbd0723c26432e9ab7d3269
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-10-29 12:26:37 +0900

    出品画面へ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :info, :price, :category_id, :condition_id, :fee_id, :prefecture_id, :scheduled_id, :image).merge(user_id: current_user.id)
+  end
 end
