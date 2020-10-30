0f599db760208f895b39c3e962408ed71cb3d226
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-10-30 14:56:07 +0900

    価格のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,17 +12,21 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
-    validates :price
     validates :category_id
     validates :condition_id
     validates :fee_id
     validates :prefecture_id
     validates :scheduled_id
     validates :image
+    validates :price
   end
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :scheduled_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1, message: 'Select'}
+    validates :condition_id, numericality: { other_than: 1, message: 'Select'}
+    validates :fee_id, numericality: { other_than: 1, message: 'Select'}
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select'}
+    validates :scheduled_id, numericality: { other_than: 1, message: 'Select'}
+    
+    with_options format: { with: /\A[3-9][0-9]{2}|[1-9][0-9]{3,6}+\z/, message: 'Out of setting range' } do
+      validates :price
+    end
 end
