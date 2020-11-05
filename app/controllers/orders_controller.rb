0c84135f730f502ef8c20a51fff5ed04b9f1c17c
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-06 00:47:09 +0900

    決済機能のサーバーサイド実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,11 @@ class OrdersController < ApplicationController
   end
 
   def create
+    binding.pry
     @item_order = ItemOrder.new(item_order_params)
     @item = Item.find(params[:item_id])
     if @item_order.valid?
+      pay_item
       @item_order.save
       redirect_to root_path
     else
@@ -17,6 +19,15 @@ class OrdersController < ApplicationController
 
   private
   def item_order_params
-    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_113461590d1facd791d7cb64"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: item_order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
