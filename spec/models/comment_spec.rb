0054a030432c03dfaebbd3cd48859bcf70db0b48
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-07 18:17:17 +0900

    単体テストコードの実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
-
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品へのコメントを投稿' do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    context 'コメントができるとき' do
+      it '必要な情報が存在するとき' do
+        expect(@comment).to be_valid
+      end
+    end
+    context 'コメントができないとき' do
+      it 'コメント内容が空だとコメントできない' do
+        @comment.comment = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Comment can't be blank")
+      end
+      it 'user_idが空だとコメントできない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("User must exist")
+      end
+      it 'item_idが空だとコメントできない' do
+        @comment.item = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Item must exist")
+      end
+    end
+  end
 end
