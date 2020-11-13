22637e935fc99163c33e911635376d9d5782841d
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-14 01:44:11 +0900

    エラーメッセージを日本語化

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -4,11 +4,11 @@ class ItemOrder
 
   with_options presence: true do
     validates :token
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}+\z/, message: 'Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}+\z/, message: 'はハイフンを含む必要があります' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
     validates :city
     validates :address
-    validates :phone, format: { with: /\A[0-9]+\z/, message: 'number Input only number' }
+    validates :phone, format: { with: /\A[0-9]+\z/, message: 'は数字のみを入力してください' }
     validates :user_id
     validates :item_id
   end
