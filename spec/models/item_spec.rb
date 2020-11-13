e97ee7bac7f4552c0f03e86e9cbd75477f3df633
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-14 02:11:23 +0900

    itemモデルの単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,92 +14,92 @@ describe Item do
       it '商品画像が空だと出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("出品画像を入力してください")
       end
       it '商品名が空だと出品できない' do
         @item.name = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it '商品説明が空だと出品できない' do
         @item.info = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Info can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       it 'カテゴリーが選択されていないと出品できない' do
         @item.category_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category Select')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it 'category_idの値が1のとき出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category Select')
+        expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
       end
       it '商品の状態が選択されていないと出品できない' do
         @item.condition_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition Select')
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
       end
       it 'condition_idの値が1のとき出品できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition Select')
+        expect(@item.errors.full_messages).to include('商品の状態を選択してください')
       end
       it '配送料の負担が選択されていないと出品できない' do
         @item.fee_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Fee Select')
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
       end
       it 'fee_idの値が1のとき出品できない' do
         @item.fee_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Fee Select')
+        expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
       end
       it '発送元が選択されていないと出品できない' do
         @item.prefecture_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture Select')
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
       end
       it 'prefecture_idの値が1のとき出品できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture Select')
+        expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
       end
       it '発送までの日数が選択されていないと出品できない' do
         @item.scheduled_id = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Scheduled Select')
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
       end
       it 'scheduled_idの値が1のとき出品できない' do
         @item.scheduled_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Scheduled Select')
+        expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
       end
       it '価格が空だと出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank")
+        expect(@item.errors.full_messages).to include("販売価格を入力してください")
       end
       it '価格の値が範囲外(下限)だと出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range')
+        expect(@item.errors.full_messages).to include('販売価格は(¥300〜9,999,999)内にしてください')
       end
       it '価格の値が範囲外(上限)だと出品できない' do
         @item.price = 100_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range')
+        expect(@item.errors.full_messages).to include('販売価格は(¥300〜9,999,999)内にしてください')
       end
       it '価格が全角だと保存できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price Out of setting range')
+        expect(@item.errors.full_messages).to include('販売価格は(¥300〜9,999,999)内にしてください')
       end
       it 'ユーザーが紐づいていないとツイートが保存できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include('Userを入力してください')
       end
     end
   end
