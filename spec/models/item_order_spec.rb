d2b307cdb7a87a3056938aaa16e4838013be39d5
IguchiRyo622 <ryou.shirakumo@ezweb.ne.jp>
2020-11-14 02:17:33 +0900

    item_orderモデルの単体テストコード修正

--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -18,62 +18,62 @@ RSpec.describe ItemOrder, type: :model do
       it 'tokenが空だと購入できない' do
         @item_order.token = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Token can't be blank")
+        expect(@item_order.errors.full_messages).to include('クレジットカード情報を入力してください')
       end
       it '郵便番号が空では購入できない' do
         @item_order.postal_code = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Postal code can't be blank")
+        expect(@item_order.errors.full_messages).to include('郵便番号を入力してください')
       end
       it '郵便番号にハイフン(-)がないと購入できない' do
         @item_order.postal_code = '1234567'
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include('Postal code Include hyphen(-)')
+        expect(@item_order.errors.full_messages).to include('郵便番号はハイフンを含む必要があります')
       end
       it '都道府県が選択されていないと購入できない' do
         @item_order.prefecture_id = ''
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item_order.errors.full_messages).to include('都道府県を選択してください')
       end
       it 'prefecture_idの値が１のとき購入できない' do
         @item_order.prefecture_id = 1
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item_order.errors.full_messages).to include('都道府県を選択してください')
       end
       it '市区町村が空だと購入できない' do
         @item_order.city = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("City can't be blank")
+        expect(@item_order.errors.full_messages).to include('市区町村を入力してください')
       end
       it '番地が空だと購入できない' do
         @item_order.address = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Address can't be blank")
+        expect(@item_order.errors.full_messages).to include('番地を入力してください')
       end
       it '電話番号が空だと購入できない' do
         @item_order.phone = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Phone can't be blank")
+        expect(@item_order.errors.full_messages).to include('電話番号を入力してください')
       end
       it '電話番号に全角だと購入できない' do
         @item_order.phone = '０００００００００００'
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include('Phone number Input only number')
+        expect(@item_order.errors.full_messages).to include('電話番号は数字のみを入力してください')
       end
       it '電話番号にハイフン(-)が入ると購入できない' do
         @item_order.phone = '000-0000-0000'
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include('Phone number Input only number')
+        expect(@item_order.errors.full_messages).to include('電話番号は数字のみを入力してください')
       end
       it 'user_idが空だと購入できない' do
         @item_order.user_id = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("User can't be blank")
+        expect(@item_order.errors.full_messages).to include('Userを入力してください')
       end
       it 'item_idが空だと購入できない' do
         @item_order.item_id = nil
         @item_order.valid?
-        expect(@item_order.errors.full_messages).to include("Item can't be blank")
+        expect(@item_order.errors.full_messages).to include('Itemを入力してください')
       end
     end
   end
